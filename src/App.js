6213ea5dcd90c75f0e93027616f2d29b3ae99106
julbar22 <julian318barbosa@gmail.com>
2021-02-09 20:06:56 -0300

    Se realizan los calculos de la cotizacion, y se muestran en pantalla

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import React, { useState } from 'react';
 import Header from './components/Header';
 import styled from '@emotion/styled';
 import Formulario from './components/Formulario'
+import Resumen from './components/Resumen'
+import Resultado from './components/Resultado'
 
 const Contenedor = styled.div`
   max-width: 600px;
@@ -13,11 +16,29 @@ const ContenedorFormulario = styled.div`
 `
 
 function App() {
+
+  const [resumen, guardarResumen] = useState({
+    cotizacion: 0,
+    datos: {
+      marca: '',
+      year: '',
+      plan: ''
+    }
+  });
+
+  const { cotizacion, datos } = resumen;
+
   return (
     <Contenedor>
       <Header titulo={'Cotizador'} />
       <ContenedorFormulario>
-        <Formulario />
+        <Formulario  guardarResumen={guardarResumen} />
+        <Resumen 
+              datos={datos}
+            />
+           <Resultado 
+                  cotizacion={cotizacion}
+                />
       </ContenedorFormulario>
     </Contenedor>
 
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styled from '@emotion/styled';
+import { obtenerDiferenciaYear,calcularMarca,obtenerPlan } from './helper';
 
 const Campo = styled.div`
     display: flex;
@@ -37,7 +38,16 @@ const Boton = styled.button`
     }
 `;
 
-const Formulario = () => {
+const Error = styled.div`
+    background-color: red;
+    color: white;
+    padding: 1rem;
+    width:100%;
+    text-align: center;
+    margin-bottom: 2rem;
+`;
+
+const Formulario = ({guardarResumen}) => {
 
     const [cotizador,setCotizador] = useState({
         marca: '',
@@ -45,17 +55,55 @@ const Formulario = () => {
         plan:''
     });
 
+    const [ error, guardarError ] = useState(false);
+
     const updateCotizador = (e) =>{
-        console.log(e)
         let nuevoCotizador = {...cotizador,
             [e.target.name]:e.target.value}
         setCotizador(nuevoCotizador);
+    }
+
+    const cotizarSeguro = e => {
+        e.preventDefault();
+
+        if(marca.trim() === '' || year.trim() === '' || plan.trim() === '') {
+            guardarError(true);
+            return;
+        }
+
+        guardarError(false);
+
+        // Una base de 2000
+        let resultado = 2000;
+
+        // obtener la diferencia de años
+        const diferencia = obtenerDiferenciaYear(year);
+
+        // por cada año hay que restar el 3%
+        resultado -= (( diferencia * 3 ) * resultado) / 100;
+
+        // Americano 15
+        // Asiatico 5%
+        // Europeo 30%
+        resultado = calcularMarca(marca) * resultado;
+
+        // Basíco aumenta 20%
+        // Completo 50%
+        const incrementoPlan = obtenerPlan(plan);
+        resultado = parseFloat( incrementoPlan * resultado ).toFixed(2);
+        
+        guardarResumen({
+            cotizacion: Number(resultado),
+            datos:cotizador
+        });
+
     }
     
 
     let {marca,year,plan} = cotizador;
     return (
-        <form>
+        <form onSubmit={cotizarSeguro}>
+            { error ? <Error>Todos los campos son obligatorios</Error>  : null }
             <Campo>
                 <Label>Marca</Label>
                 <Select
